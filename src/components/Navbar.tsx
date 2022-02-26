7031b7551f29b411a815d895e72234b2a3891ac7
Nitish <tnitish654@gmail.com>
2022-02-26 19:02:41 +0800

    Logo on "navbar" brings you to home on click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,23 @@
 import { Box, Center, Icon, Image } from "@chakra-ui/react"
-import React from "react"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
+	const navigate = useNavigate()
+
 	return (
-		<Center h="6vh" mb="2.5vh" w="100%" bgColor="hsl(240, 3%, 12%)" objectFit="contain">
-			<Image h={8} src="./assets/Alprom-logo.svg" />
+		<Center
+			h="6vh"
+			mb="2.5vh"
+			w="100%"
+			bgColor="hsl(240, 3%, 12%)"
+			objectFit="contain">
+			<Image
+				h={8}
+				src="./assets/Alprom-logo.svg"
+				transition="all 0.3s ease-out"
+				_hover={{ opacity: 0.7, cursor: "pointer" }}
+				onClick={() => navigate("/")}
+			/>
 		</Center>
 	)
 }
