f74b817ed21f8b016ec417166e75dcc1df520d40
Joey Lim <jyorin0206@gmail.com>
2022-02-26 23:59:17 +0800

    dynamic multiplayer

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -7,75 +7,81 @@ const Game = () => {
 	const testArray = Array(30).fill(0)
 
 	const wordArray = ["B", "A", "L", "L", "S", "T", "E", "S", "T", "S"]
+	
+	const dummyData: any = {
+        putt: {
+            points: 0,
+            round: 0,
+            guesses: [
+                0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ],
+        },
+        joey: {
+            points: 0,
+            round: 0,
+            guesses: [
+                0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ],
+        },
+        zec: {
+            points: 0,
+            round: 0,
+            guesses: [
+                0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ],
+        },
+        nitish: {
+            points: 0,
+            round: 0,
+            guesses: [
+                0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ],
+        },
+
+
+    }
+
 
 	return (
 		<>
 			<Center>
-				<SimpleGrid >
+				<SimpleGrid columns={2}  >
 
 				{/* left side players */}
-				<SimpleGrid columns={2} columnGap={8} rowGap={4} paddingRight={8}>
-				<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
+				<SimpleGrid columns={2} columnGap={4} rowGap={8} paddingRight={8}>
+					{
+						Object.keys(dummyData).map((key, index) => {
+							const guesses = dummyData[key].guesses
+							return <Grid templateColumns="repeat(5, min-content)" gap={1.5}>
+							{Array(30)
+								.fill(0)
+								.map((_, i) => (
+									<LetterBox
+										key={i}
+										state={guesses[i]}
+										letter={wordArray[i]}
+										isSmall={true}
+									/>
+								))}
+						</Grid>
+						})
+					}
 
-					<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
 
-					<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
-
-					<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
 			</SimpleGrid>
-											
+			
 				</SimpleGrid>
 
 			{/* main grid and keyboard */}
 			<Center
 				flexDirection="column"
 				h="90vh"
-				justifyContent="space-between">
-				<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
+				>
+				<Grid templateColumns="repeat(5, min-content)" gap={1.5} marginBottom={10}>
 					{Array(30)
 						.fill(0)
 						.map((_, i) => (
@@ -87,62 +93,7 @@ const Game = () => {
 						))}
 				</Grid>
 				<Keyboard />
-			</Center>
-
-				{/* right side players */}
-				<SimpleGrid columns={2} columnGap={8} rowGap={4} paddingLeft={8}>
-				<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
-
-					<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
-
-					<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
-
-					<Grid templateColumns="repeat(5, min-content)" gap={1.5}>
-						{Array(30)
-							.fill(0)
-							.map((_, i) => (
-								<LetterBox
-									key={i}
-									state={testArray[i]}
-									letter={wordArray[i]}
-									isSmall={true}
-								/>
-							))}
-					</Grid>
-			</SimpleGrid>
+			</Center>	
 
 			</Center>
 
