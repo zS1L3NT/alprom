1d9d73f106aaa5d6e44c13c123bbddb39d4ba18d
Zechariah <zechariahtan144@gmail.com>
2022-02-26 14:23:44 +0800

    Revert "Added next work api"
    This reverts commit eb221189019dd8be5f26c5d1db7d6309ddb0d777.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,8 @@
 import letters from "./slices/letters"
 import { configureStore } from "@reduxjs/toolkit"
-import { wordApi } from "./api/word"
 
 export const store = configureStore({
-	reducer: {
-		[wordApi.reducerPath]: wordApi.reducer,
-		letters
-	},
+	reducer: { letters },
 })
 
 export type RootState = ReturnType<typeof store.getState>
