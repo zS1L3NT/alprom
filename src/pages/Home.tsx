ae7e8e600aabe9822b9b435a0581a9112aaa1a20
Zechariah <zechariahtan144@gmail.com>
2022-02-26 22:43:09 +0800

    Refactored error cases

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -59,7 +59,11 @@ const Home = () => {
 	const joinRoom = async () => {
 		const collRef = collection(firestore, "rooms")
 		const docs = await getDocs(query(collRef, where("code", "==", roomId)))
-		if (docs.docs.length === 1) {
+		if (docs.docs.length !== 1) {
+			// ! Handle no room found error
+		} else if (username in docs.docs[0]!.data().scores) {
+			// ! Handle username taken error
+		} else {
 			const docRef = doc(collRef, docs.docs[0]!.id)
 			setDoc(
 				docRef,
@@ -75,8 +79,6 @@ const Home = () => {
 				{ merge: true },
 			)
 			navigate("/lobby")
-		} else {
-			// ! Handle
 		}
 	}
 
