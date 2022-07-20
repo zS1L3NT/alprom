28691f87430e963f37bbd26d1b28212be74588da
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-20 16:37:13 +0800

    🔥 remove rtk hooks

--- src/hooks/useAppDispatch.tsx
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from "react-redux"
-
-/**
- * The useDispatch() hook configured with the app's dispatch type
- */
-export const useAppDispatch = () => useDispatch<AppDispatch>()
--- src/hooks/useAppSelector.tsx
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux"
-
-/**
- * An enhanced selector configured for the app's state.
- */
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
