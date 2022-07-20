628fbcc9d7f142a8a649d2114bdb97a0c8a8de45
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-20 22:22:07 +0800

    ✨ got keyboard to type letters

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,4 +1,4 @@
-import { doc, DocumentReference, onSnapshot } from "firebase/firestore"
+import { doc, DocumentReference, onSnapshot, updateDoc } from "firebase/firestore"
 import { FC, PropsWithChildren, useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 
@@ -18,7 +18,9 @@ const Game: FC<PropsWithChildren<{}>> = props => {
 	const [roomRef, setRoomRef] = useState<DocumentReference<iRoom> | null>(null)
 	const [room, setRoom] = useState<iRoom | null>(null)
 	const [word, setWord] = useState<string | null>(null)
-	const [letters, setLetters] = useState<string[][]>([])
+	const [letters, setLetters] = useState<string[][]>([[]])
+
+	const alphabet = Array.from(Array(26)).map((_, i) => String.fromCharCode(i + 65))
 
 	useEffect(() => {
 		if (username === null) {
@@ -47,8 +49,6 @@ const Game: FC<PropsWithChildren<{}>> = props => {
 	useEffect(() => {
 		if (roomRef === null || username === null) return
 
-		console.log("snap")
-
 		return onSnapshot(roomRef, doc => {
 			if (doc.exists()) {
 				const room = doc.data()
@@ -76,6 +76,62 @@ const Game: FC<PropsWithChildren<{}>> = props => {
 		})
 	}, [roomRef, username])
 
+	useEffect(() => {
+		if (roomRef === null || room === null || username === null || word === null) return
+
+		const handler = async (e: KeyboardEvent) => {
+			switch (e.key) {
+				case "Backspace":
+					setLetters(letters => {
+						if (letters.length === 0) return [[]]
+
+						const row = letters.at(-1)!
+						if (row.length === 1) return [...letters.slice(0, -1), []]
+
+						return [...letters.slice(0, -1), row.slice(0, -1)]
+					})
+					break
+				case "Enter":
+					setLetters(letters => {
+						if (letters.at(-1)!.length === 5) {
+							updateDoc(roomRef, `game.${username}.${word}`, [
+								...room!.game[username]![word]!,
+								...calculate(word!, letters.at(-1)!.join(""))
+							])
+							return [...letters, []]
+						} else {
+							return letters
+						}
+					})
+					break
+				default:
+					if (alphabet.includes(e.key.toUpperCase())) {
+						setLetters(letters => {
+							if (letters.length === 1) {
+								if (letters[0]!.length === 0) return [[e.key.toLowerCase()]]
+								if (letters[0]!.length === 5) return letters
+								return [[...letters[0]!, e.key.toLowerCase()]]
+							}
+
+							if (letters.at(-1)!.length === 5) return letters
+
+							return [
+								...letters.slice(0, -1),
+								[...letters.at(-1)!, e.key.toLowerCase()]
+							]
+						})
+					}
+					break
+			}
+		}
+
+		window.addEventListener("keydown", handler)
+
+		return () => {
+			window.removeEventListener("keydown", handler)
+		}
+	}, [roomRef, room, username, word])
+
 	if (username === null || room === null || word === null) {
 		return <Spinner />
 	}
@@ -88,29 +144,31 @@ const Game: FC<PropsWithChildren<{}>> = props => {
 					columnGap={4}
 					rowGap={8}
 					paddingRight={8}>
-					{Object.entries(room.game ?? {}).map(([username, data]) => {
-						const word = room.words[Object.keys(data).length - 1]!
-						const guesses = [
-							...data[word]!,
-							...Array.from<null>(Array(30 - data[word]!.length * 5)).fill(null)
-						]
+					{Object.entries(room.game ?? {})
+						.filter(entry => entry[0] !== username)
+						.map(([username, data]) => {
+							const word = room.words[Object.keys(data).length - 1]!
+							const guesses = [
+								...data[word]!,
+								...Array.from<null>(Array(30 - data[word]!.length)).fill(null)
+							]
 
-						return (
-							<Grid
-								key={username}
-								templateColumns="repeat(5, min-content)"
-								gap={1.5}>
-								{guesses.map((guess, i) => (
-									<LetterSquare
-										key={i}
-										guess={guess}
-										letter=""
-										isSmall={true}
-									/>
-								))}
-							</Grid>
-						)
-					})}
+							return (
+								<Grid
+									key={username}
+									templateColumns="repeat(5, min-content)"
+									gap={1.5}>
+									{guesses.map((guess, i) => (
+										<LetterSquare
+											key={i}
+											guess={guess}
+											letter=""
+											isSmall={true}
+										/>
+									))}
+								</Grid>
+							)
+						})}
 				</SimpleGrid>
 			</SimpleGrid>
 			<Center
@@ -122,9 +180,9 @@ const Game: FC<PropsWithChildren<{}>> = props => {
 					marginBottom={5}>
 					{[...letters, ...Array.from<null>(Array(6 - letters.length)).fill(null)].map(
 						(row, i) => {
-							if (row?.every(letter => letter !== null)) {
-								const result = calculate(word!, row!.join(""))
-								return result.map((guess, i) => (
+							const guesses = room.game[username]![word]!.slice(i * 5, i * 5 + 5)
+							if (row && row.length === 5 && guesses.length === 5) {
+								return guesses.map((guess, i) => (
 									<LetterSquare
 										key={i}
 										guess={guess}
@@ -132,12 +190,12 @@ const Game: FC<PropsWithChildren<{}>> = props => {
 										isSmall={false}
 									/>
 								))
-							} else if (row?.some(letter => letter !== null)) {
-								return row!.map((letter, i) => (
+							} else if (row && row.length > 0) {
+								return Array.from(Array(5)).map((_, i) => (
 									<LetterSquare
 										key={i}
 										guess={null}
-										letter={letter}
+										letter={row[i] ?? ""}
 										isSmall={false}
 									/>
 								))
