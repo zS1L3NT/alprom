975c7aeed71194242e148cf8ac85c4fece22c2fc
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-21 10:23:12 +0800

    🐛 fix loader height bug

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -127,74 +127,76 @@ const Lobby = () => {
 	}
 
 	return (
-		<Center flexDir="column">
-			<VStack
-				mb="2em"
-				spacing={0}>
-				<Text
-					fontSize="4xl"
-					fontWeight="semibold">
-					Room Id
-				</Text>
-				<Skeleton isLoaded={!!room?.code}>
+		<>
+			<Center flexDir="column">
+				<VStack
+					mb="2em"
+					spacing={0}>
 					<Text
-						fontSize="3xl"
+						fontSize="4xl"
 						fontWeight="semibold">
-						{room ? room.code : "000000"}
+						Room Id
 					</Text>
-				</Skeleton>
-			</VStack>
-			<VStack mb="5em">
-				<Text
-					textDecoration="underline"
-					fontSize="2xl"
-					fontWeight="semibold">
-					People in this lobby
-				</Text>
-				<OrderedList>
-					{Object.keys(room?.game || {})
-						.sort()
-						.map((name, index) => (
-							<ListItem
-								key={index}
-								fontSize="xl"
-								_hover={{
-									textDecoration: "line-through",
-									textDecorationThickness: "3px",
-									cursor: "pointer"
-								}}>
-								{name}
-							</ListItem>
-						))}
-				</OrderedList>
-			</VStack>
-			<Tooltip
-				label="Only the room owner can start the game"
-				shouldWrapChildren
-				mb={2}
-				placement="top"
-				isDisabled={room?.owner === username}>
+					<Skeleton isLoaded={!!room?.code}>
+						<Text
+							fontSize="3xl"
+							fontWeight="semibold">
+							{room ? room.code : "000000"}
+						</Text>
+					</Skeleton>
+				</VStack>
+				<VStack mb="5em">
+					<Text
+						textDecoration="underline"
+						fontSize="2xl"
+						fontWeight="semibold">
+						People in this lobby
+					</Text>
+					<OrderedList>
+						{Object.keys(room?.game || {})
+							.sort()
+							.map((name, index) => (
+								<ListItem
+									key={index}
+									fontSize="xl"
+									_hover={{
+										textDecoration: "line-through",
+										textDecorationThickness: "3px",
+										cursor: "pointer"
+									}}>
+									{name}
+								</ListItem>
+							))}
+					</OrderedList>
+				</VStack>
+				<Tooltip
+					label="Only the room owner can start the game"
+					shouldWrapChildren
+					mb={2}
+					placement="top"
+					isDisabled={room?.owner === username}>
+					<Button
+						size="lg"
+						w="lg"
+						mb={5}
+						isDisabled={room?.owner !== username}
+						bgColor="correct"
+						_hover={{ bgColor: "hsl(115, 29%, 35%)" }}
+						_active={{ bgColor: "hsl(115, 29%, 30%)" }}
+						onClick={startGame}>
+						Start Game
+					</Button>
+				</Tooltip>
 				<Button
-					size="lg"
-					w="lg"
-					mb={5}
-					isDisabled={room?.owner !== username}
-					bgColor="correct"
-					_hover={{ bgColor: "hsl(115, 29%, 35%)" }}
-					_active={{ bgColor: "hsl(115, 29%, 30%)" }}
-					onClick={startGame}>
-					Start Game
+					size="md"
+					w="xs"
+					bgColor="hsl(0, 70%, 53%)"
+					_hover={{ bgColor: "hsl(0, 70%, 45%)" }}
+					_active={{ bgColor: "hsl(0, 70%, 40%)" }}
+					onClick={room?.owner === username ? closeRoom : leaveRoom}>
+					{room?.owner === username ? "Close room" : "Leave room"}
 				</Button>
-			</Tooltip>
-			<Button
-				size="md"
-				w="xs"
-				bgColor="hsl(0, 70%, 53%)"
-				_hover={{ bgColor: "hsl(0, 70%, 45%)" }}
-				_active={{ bgColor: "hsl(0, 70%, 40%)" }}
-				onClick={room?.owner === username ? closeRoom : leaveRoom}>
-				{room?.owner === username ? "Close room" : "Leave room"}
-			</Button>
+			</Center>
 			<Fade in={isLoading}>
 				<Center
 					pos="absolute"
@@ -207,7 +209,7 @@ const Lobby = () => {
 					<Spinner />
 				</Center>
 			</Fade>
-		</Center>
+		</>
 	)
 }
 
